b908d035ddfe52111862a538439314c455191686
Jennifer Stewart <jstewart8053@gmail.com>
2020-08-31 22:55:06 -0500

    just need to add completed button functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import TodoList from "./components/TodoList"
+import TodoForm from "./components/TodoForm"
+import "./styles.css";
+
+
+const listData =
+[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      list: listData,
+      toggleCompleted: () => {}
+    };
   }
-}
 
-export default App;
+ toggleCompleted = (clickedItemId) => {
+   this.setState({
+     list: this.state.list.map((item) => {
+       if(item.id === clickedItemId) {
+         return {
+           ...item,
+           completed: !item.completed
+         };
+         
+       } else {
+         return item;
+       }
+     })
+   })
+ }
+ //addItem property
+ addItem = (itemTask) => {
+   const newTask = {
+     task: itemTask,
+     id: new Date(),
+     completed: false
+   };
+this.setState({
+  list: [...this.state.list, newTask]
+})
+ }
+ render() {
+   return (
+     <div className = 'App'>
+       <div className = 'header'>
+         <h1>ToDo List </h1>
+         <TodoForm addItem = {this.addItem} />
+         </div>
+         <TodoList
+         list = {this.state.list}
+         toggleCompleted = {this.toggleCompleted}
+         />
+         </div>
+   )
+ }
+}
+ 
+
+export default App
+
+// `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on `<App />`.
+//   - All of your `handler` functions should live here on `<App />`.
---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <div onClick = { () => props.toggleCompleted(props.todo.id)} className = {`todo${props.todo.completed ? 'completed' : ''}`}>
+    <p> { props.todo.task } </p>
+    </div>
+    )
+}
+
+export default Todo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  `<Todo />` is a component that takes in the `todo` data and displays the task to the screen.
